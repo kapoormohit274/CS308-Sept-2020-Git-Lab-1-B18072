8c15f4186b3b424448c1db78904d30f04aedae36
kapoormohit274 <43112120+kapoormohit274@users.noreply.github.com>
2020-10-15 14:43:24 +0530

    add validation of no

--- PartA/sqrt.c
+++ PartA/sqrt.c
@@ -9,7 +9,12 @@ int main(int argc, char* argv[]) {
 		exit(-1);
 	}
 
-	int input = atoi(argv[1]);
+	int input = atoi(argv[1]);	
+	if(input < 0){
+		printf("Not a valid number");
+		exit(-1);
+	}
+
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.");
 	return(0);
